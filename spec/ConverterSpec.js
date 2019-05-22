031ea7fab186ccea1e9d7afc2aca2c2d3917b4a1
chipo <chipomapondera@gmail.com>
2019-05-22 20:22:26 +0100

    Hello

---
+++ spec/ConverterSpec.js
@@ -0,0 +1,20 @@
+describe("Cooking Ingredients Converter", function() {
+
+  describe("Temperatures", function() {
+    it("Fahrenheit to Celsius", function() {
+      var result = convertFahrenheitToCelsius(100);
+
+      expect(result).toEqual('37.8');
+    });
+
+   	it("Celsius to Fahrenheit", function() {
+   		var result = convertCelsiusToFahrenheit(37.8);
+
+   		expect(result).toEqual('100');
+   	}); 
+  });
+});
+
+
+
+
---
+++ src/Converter.js
@@ -0,0 +1,13 @@
+/** Unit Converter **/
+function convertFahrenheitToCelsius(temperature) {
+  //conversion code
+  var celsius = (temperature - 32)/1.8
+  return celsius.toFixed(1);
+}
+
+function convertCelsiusToFahrenheit(temperature) {
+	var fahrenheit = temperature * 1.8 + 32
+	return fahrenheit.toFixed(0);
+}
+
+
