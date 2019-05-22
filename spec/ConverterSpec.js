ed5542ac6a049d8ca4cca840d3cbfa0f8211a5c6
chipo <chipomapondera@gmail.com>
2019-05-22 21:10:23 +0100

    Hello again

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -11,8 +11,32 @@ describe("Cooking Ingredients Converter", function() {
    		var result = convertCelsiusToFahrenheit(37.8);
 
    		expect(result).toEqual('100');
-   	}); 
+   	});
   });
+
+  describe("Weight", function() {
+  	it("Pounds to Kilos", function() {
+  		var result = convertPoundsToKilos(10);
+
+  		expect(result).toEqual('4.5');
+  	});
+  }); 
+
+  describe("Volume", function() {
+  	it("Litre to Gallons", function() {
+  		var result = convertLitresToGallons(10);
+
+  		expect(result).toEqual('2.20');
+  	});
+  });
+
+  describe("Distance", function() {
+  	it("Miles to Km", function() {
+  		var result = convertMilesToKm(10);
+
+  		expect(result).toEqual('16');
+  	});
+  })
 });
 
 
--- src/Converter.js
+++ src/Converter.js
@@ -10,4 +10,17 @@ function convertCelsiusToFahrenheit(temperature) {
 	return fahrenheit.toFixed(0);
 }
 
+function convertPoundsToKilos(weight) {
+	var kilo = weight * 0.4536
+	return kilo.toFixed(1); 
+}
 
+function convertLitresToGallons(volume) {
+	var gallons = volume * 0.22
+	return gallons.toFixed(2);
+}
+
+function convertMilesToKm(distance) {
+	var kms = distance * 1.609
+	return kms.toFixed(0);
+}
